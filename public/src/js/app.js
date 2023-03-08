018027f9aaefdcd32dbdf7bd1f8906f6be607f48
matiasgql <matiasql999@gmail.com>
2023-03-08 11:01:26 -0300

    movi los sliders y las tarjetas a ejs

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -2,19 +2,74 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const port = 4060
-let datosHome = []
-let datosAnimales = require("../data/datosAnimales.json")
-
-datosHome.push({
-    categoriaNombre: "Animales",
-    datosAnimales: datosAnimales
-})
+let datosHome = [
+    {
+        categoriaNombre: "Animales",
+        categoriaImg: "../../images/Categoria_Animales.jpg",
+        datosProductos: [
+            {
+                id: 1,
+                name: "Animal1",
+                img: "../../images/producto.jpg",
+                price: 20,
+            },
+            {
+                id: 2,
+                name: "Animal2",
+                img: "../../images/producto.jpg",
+                price: 50,
+            },
+            {
+                id: 3,
+                name: "Animal3",
+                img: "../../images/producto.jpg",
+                price: 40,
+            },
+            {
+                id: 4,
+                name: "Animal4",
+                img: "../../images/producto.jpg",
+                price: 80,
+            },
+        ]
+    },
+    {
+        categoriaNombre: "Paisajes",
+        categoriaImg: "../../images/Categoria_Animales.jpg",
+        datosProductos: [
+            {
+                id: 1,
+                name: "Paisaje1",
+                img: "../../images/producto.jpg",
+                price: 10,
+            },
+            {
+                id: 2,
+                name: "Paisaje2",
+                img: "../../images/producto.jpg",
+                price: 120,
+            },
+            {
+                id: 3,
+                name: "Paisaje3",
+                img: "../../images/producto.jpg",
+                price: 160,
+            },
+            {
+                id: 4,
+                name: "Paisaje4",
+                img: "../../images/producto.jpg",
+                price: 200,
+            },
+        ]
+    },
+]
 
 app.use(express.static(path.join(__dirname,"../../../public")))
 
 app.listen(port,()=>console.log("El servidor " + port + " se levanto"))
 
-app.get("/", (req, res) => {res.render(path.join(__dirname,"../views/home.ejs"), {datosHome})});//res.sendFile(path.join(__dirname,"../views/home"))})
+app.get("/", (req, res) => {res.render(path.join(__dirname,"../views/home.ejs"), {datosHome})});
 
 app.get("/register", (req, res) => {res.sendFile(path.join(__dirname,"../views/register.html"))})
 
